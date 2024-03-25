c0450dc7d6d167d740226a9986e0570f6be5c84e
ZhannaSaltanova <nedikovazhanna@gmail.com>
2024-03-25 14:40:51 +0200

    first commit

---
+++ script/script.js
@@ -0,0 +1,31 @@
+const swiper = new Swiper('.swiper', {
+    slidesPerView: 3, 
+    spaceBetween: 5, 
+    slidesPerGroup: 3, 
+    autoplay: {
+        delay: 4000, 
+        disableOnInteraction: false, 
+      },
+    loop:true,
+    breakpoints: {
+      1: {
+        slidesPerView: 1, 
+        spaceBetween: 10, 
+        slidesPerGroup: 1, 
+      },
+      500: {
+        slidesPerView: 2, 
+        spaceBetween: 10, 
+        slidesPerGroup: 1, 
+      },
+      768: {
+        slidesPerView: 2, 
+        spaceBetween: 10, 
+        slidesPerGroup: 1, 
+      },
+      1024: { 
+        slidesPerView: 3, 
+        spaceBetween: 15  
+      }
+    },
+  });
